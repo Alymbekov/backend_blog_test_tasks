2fb2be3fbde525ea3bca21e59d5df27627e25383
Alymbekov <maxim.makarov.1997@mail.ru>
2020-11-22 23:30:13 +0600

    Add follow functionality and ListView querysets logic

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,9 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.IndexPageView.as_view(), name='index'),
     path('follow-blog/', views.FollowBlogView.as_view(), name='follow_blog'),
-    path('all-blogs/', views.BlogListView.as_view(), name='all_blogs'),
+    path('all-blog/', views.BlogListView.as_view(), name='all_blog'),
+    path('add-follow/', views.FollowerView.as_view(), name='add_follow'),
+    path('my-blog/', views.MyBlogView.as_view(), name='my_blog'),
+
 ]
 
